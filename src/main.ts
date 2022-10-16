964bd5b512400ae0169ebd384bf5423072b5427f
Karol Gruszka <37630454+karolgruszka93@users.noreply.github.com>
2022-10-16 13:53:48 +0200

    feature/add-https-protocol-config (#7)
    * enable https, update .gitignore, update config
    
    * update cert and key path

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync(__dirname + '/secrets/localhost-key.pem'),
+  cert: fs.readFileSync(__dirname + '/secrets/localhost.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
